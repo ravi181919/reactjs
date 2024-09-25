9f5016c3c1a0e92486c07cf847ed82c95e9afcca
ravigupta <rg187888@gmail.com>
2024-09-25 16:43:39 +0530

    context api project done

--- 10_contextAPi/project/src/main.jsx
+++ 10_contextAPi/project/src/main.jsx
@@ -2,12 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeContextProvider } from "./context/ThemeContext.js";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   
-      <App />
-    
+    <App />
   </StrictMode>
 );
